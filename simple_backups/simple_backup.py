b1c66f5f69d6c47b7f08381c383641d8297ef3a9
Joshua Coales <joshua@coales.co.uk>
2021-06-26 00:23:28 +0100

    Implementing heartbeat sending

--- simple_backups/simple_backup.py
+++ simple_backups/simple_backup.py
@@ -1,8 +1,9 @@
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Dict
 
 import schedule
+import heartbeat
 
 from simple_backups.outputs import OutputFactory
 from simple_backups.schedules import ScheduleFactory
@@ -16,8 +17,10 @@ class SimpleBackup:
         schedule_factory = ScheduleFactory()
         self.sources = [source_factory.from_json(source, schedule_factory) for source in config["sources"]]
         self.outputs = [output_factory.from_json(output) for output in config["outputs"]]
-        self.heartbeat_url = config["heartbeat_url"]
         self.running = False
+        heartbeat.heartbeat_app_url = config["heartbeat_url"]
+        self.heartbeat_id = config["heartbeat_id"]
+        heartbeat.initialise_app(self.heartbeat_id, timedelta(minutes=5))
 
     def run_backup(self, source: Source) -> None:
         timestamp = datetime.now()
@@ -26,10 +29,9 @@ class SimpleBackup:
             output.send_backup(backup_path)
 
     def send_heartbeat(self) -> None:
-        # TODO
-        raise NotImplementedError
+        heartbeat.update_heartbeat(self.heartbeat_id)
 
-    def setup_schedules(self):
+    def setup_schedules(self) -> None:
         for source in self.sources:
             source.schedule.schedule_job(lambda: self.run_backup(source))
         schedule.every(2).minutes.do(self.send_heartbeat)
